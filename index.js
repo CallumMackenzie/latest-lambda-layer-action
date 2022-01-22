919dd194124570705a6e24af5a57a45e03c82718
Callum Mackenzie <61220096+CallumMackenzie@users.noreply.github.com>
2022-01-22 14:40:49 -0700

    Go back to using function name over ARN

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const run = async () => {
 
 
 		const lambda_function_data = await lambda.getFunctionConfiguration({
-			FunctionName: "arn:aws:lambda:" + lambda_config.region + ":function:" + lambda_function_name,
+			FunctionName: lambda_function_name,
 		}).promise();
 
 		core.info("Received lambda function information.");
