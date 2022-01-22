758b1715f6deb451e5a3bc90200f99fd7dc92d31
Callum Mackenzie <Alexxander1611@gmail.com>
2022-01-22 13:17:23 -0700

    Pre-test commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Latest Lambda Layer Action
+
+This action takes given aws credentials and updates the lambda layers of the given function to their latest versions
+
+## Usage
+```yaml
+name: Update AWS Lambda Function Layers
+
+on:
+  release:
+    types: [created]
+  push:
+    branches:
+      - master
+
+jobs:
+  update_lambda_layers:
+  	name: Update lambda layer versions
+    runs-on: ubuntu-latest
+	steps:
+	  - name: Update layers
+	    uses: callummackenzie/latest-lambda-layer-action@v1
+		with:
+          aws_access_key_id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+          aws_secret_access_key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+          aws_region: ${{ secrets.AWS_REGION }
+		  function_name: SomeAWSFunction
+```
+## Parameters
+- aws_access_key_id
+  - Required
+  - The aws access key id for an IAM user
+- aws_secret_access_key
+  - Required
+  - The secret access key for the same IAM user used in aws_access_key_id
+- aws_region
+  - Required
+  - The aws region in which the desired lambda function is located such as us-east-1
+- function_name
+  - Required
+  - The lambda function name to be updated (not the ARN)
---
+++ index.js
@@ -0,0 +1,52 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+import Lambda from 'aws-sdk/clients/lambda';
+
+const run = async () => {
+	try {
+		// Extract name
+		const lambda_function_name = core.getInput('function_name', { required: true });
+		const lambda_config = {
+			accessKeyId: core.getInput('aws_access_key_id', { required: true }),
+			apiVersion: '2015-03-31',
+			maxRetries: 2,
+			region: core.getInput('aws_region', { required: true }),
+			secretAccessKey: core.getInput('aws_secret_access_key', { required: true }),
+			sslEnabled: true
+		};
+
+		// Create lambda API client
+		const lambda = new Lambda(lambda_config);
+
+		core.info("Getting lambda layers for function");
+
+
+		const lambda_function_data = await lambda.getFunction({
+			FunctionName: lambda_function_name
+		}).promise();
+
+		core.info("Received lambda function information.");
+		const new_layer_list = [];
+		lambda_function_data.Configuration.Layers.forEach((layer, index, arr) => {
+			const lambda_layer_data = await lambda.listLayerVersions({
+				LayerName: layer.Arn
+			}).promise();
+			core.info("Recieved lambda versions for layer (" + index + "/" + arr.length + ")");
+
+			const target_layer = lambda_layer_data.LayerVersions.sort((a, b) => b.Version - a.Version)[0];
+			new_layer_list.push(target_layer.LayerVersionArn);
+		});
+
+		core.info("Updating function layers");
+		const update_lambda_config = await lambda.updateFunctionConfiguration({
+			FunctionName: lambda_function_name,
+			Layers: new_layer_list
+		}).promise();
+
+		core.info("Layer update success");
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+};
+
+run();
